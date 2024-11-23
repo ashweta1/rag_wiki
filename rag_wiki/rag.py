67ce1c9b4bf5c8d188d67421d8ed9ba1847a4614
ashweta <ashweta@gmail.com>
2024-11-22 18:10:15 -0800

    Batch process texts from dataset iterator

--- rag_wiki/rag.py
+++ rag_wiki/rag.py
@@ -1,7 +1,7 @@
 import torch
 import faiss
+from itertools import islice
 from rag_wiki.util import best_device
-from rag_wiki.util import is_gpu
 from datasets import load_dataset
 from transformers import AutoModel, AutoTokenizer
 
@@ -31,62 +31,74 @@ def get_embedding_model(model_name="sentence-transformers/all-MiniLM-L6-v2"):
     return model, tokenizer
 
 
-def encode(texts, model, tokenizer, batch_size=1000, debug=False):
-    # create a list of torch tensor embeddings
-    embeddings = []
-    for i in range(0, len(texts), batch_size):
-        debug and print(f"Processing batch {i//batch_size} from {i}:{min(i + batch_size, len(texts))}")
-        batch_texts = texts[i:i + batch_size]  # get a batch of texts
-        tokens = tokenizer(batch_texts, return_tensors="pt", truncation=True, padding=True, max_length=512)
-        tokens = {key: value.to(best_device()) for key, value in tokens.items()}  # Move input tensors to GPU
+def encode(texts, model, tokenizer, debug=False):
+    tokens = tokenizer(texts, return_tensors="pt", truncation=True, padding=True, max_length=512)
+    tokens = {key: value.to(best_device()) for key, value in tokens.items()}  # Move input tensors to GPU
 
-        with torch.no_grad():
-            # Do a forward pass and use [CLS] token embeddings
-            model_output = model(**tokens)
-            batch_embeddings = model_output.last_hidden_state[:, 0, :]
-            debug and print("Batch embeddings shape: ", batch_embeddings.shape)
-            embeddings.append(batch_embeddings)
+    with torch.no_grad():
+        # Do a forward pass and use [CLS] token embeddings
+        model_output = model(**tokens)
+        embeddings = model_output.last_hidden_state[:, 0, :]
 
-    embeddings = torch.vstack(embeddings).cpu().numpy()
-    debug and print("# embeddings: ", embeddings.shape)
+    debug and print("torch embeddings tensor shape: ", embeddings.shape)
+    embeddings = embeddings.cpu().numpy()
+    debug and print("numpy embeddings shape: ", embeddings.shape)
 
     return embeddings
 
 
 def preprocess(dataset, batch_size=1000, debug=False):
-    """
-    Preprocess the dataset.
-    :param dataset:
-    :return: FAISS index
-    """
-    # print what device we have.
+     # print what device we have.
     debug and print("device = ", best_device())
 
-    # Get the list of texts from the dataset
-    debug and print("Listing text values in the dataset...")
-    texts = [str(example['text']) for example in dataset]
-    debug and print("Listing text values in the dataset... done.")
-    debug and print("List of texts size = ", len(texts))
-
-    # Encode embeddings using the model and tokenizer
-    debug and print("Encoding the dataset embeddings...")
+    # Get the model to generate embeddings
+    debug and print("Getting the model and tokenizer for embeddings...")
     model, tokenizer = get_embedding_model()
-    embeddings = encode(texts, model, tokenizer, batch_size, debug)
-    debug and print("Encoding the dataset embeddings... done.")
-    debug and print("#Dataset mbeddings = ", embeddings.shape, "d = ", embeddings.shape[1])
+    # Get the embeddings size
+    embedding_size = model.config.hidden_size
+    debug and print("Embedding size = ", embedding_size)
+    debug and print("Getting the model and tokenizer for embeddings... done")
 
-    # Add the embeddings to an index
-    debug and print("Adding embeddings to the index...")
-    index = faiss.IndexFlatL2(embeddings.shape[1])
+    # Create FAISS index
+    debug and print("Creating FAISS index for storing and searching the embeddings...")
+    index = faiss.IndexFlatL2(embedding_size)
     if best_device() == "cuda":
-        debug and print("Creating GPU faiss index")
+        print("Moving the index to GPU")
         res = faiss.StandardGpuResources()
         index = faiss.index_cpu_to_gpu(res, 0, index)
+    debug and print("Creating FAISS index... done")
 
-    index.add(embeddings)
-    debug and print("Index dimension: ", index.d)
-    debug and   print("Adding embeddings to the index... done.")
+    # Encode and store embeddings for texts in batches
+    def batched_iterable(dataset, batch_size):
+        iterator = iter(dataset)
+        while True:
+            batch = list(islice(iterator, batch_size))
+            if not batch:
+                break
+            yield batch
+    texts = []
+    for batch_idx, batch in enumerate(batched_iterable(dataset, batch_size)):
+        print(f"Batch {batch_idx + 1}")
 
+        # batch_texts = [str(example['text']) for example in islice(dataset, batch_size)]
+        batch_texts = [str(example['text']) for example in batch]
+        debug and print(f"Got {len(batch_texts)} texts")
+        debug and print("Getting batch texts... done.")
+
+        # Encode embeddings using the model and tokenizer
+        debug and print("Encoding the dataset embeddings...")
+        embeddings = encode(batch_texts, model, tokenizer, debug)
+        debug and print("Encoding the dataset embeddings... done.")
+        debug and print("#Dataset embeddings = ", embeddings.shape, "d = ", embeddings.shape[1])
+
+        # Add the embeddings to an index
+        debug and print(f"Adding embeddings to the index (dimension {index.d})...")
+        index.add(embeddings)
+        debug and print("Adding embeddings to the index... done.")
+
+        texts.extend(batch_texts)
+
+    debug and print("Length of texts = ", len(texts))  # expect total size.
     return index, texts
 
 
@@ -107,13 +119,12 @@ def retrieve(prompts, index, texts, top_k=5, debug=False):
     prompt_embeddings = encode(prompts, model, tokenizer, debug=debug)
     debug and print("Encoding prompt embeddings... done.")
     debug and print("Length of prompt_embeddings = ", len(prompt_embeddings))
-    debug and print(prompt_embeddings)
 
     # Search the index and returns (num_prompts, top_k) arrays of distances and indices
     debug and print("Searching for texts in the index...")
     debug and print("Index: ", index)
     debug and print("Index dimension: ", index.d)
-    debug and print("Query vectors: ", prompt_embeddings)
+    # debug and print("Query vectors: ", prompt_embeddings)
     debug and print("Top k ", top_k)
     _, indices = index.search(prompt_embeddings, top_k)
     debug and print("Search for texts in the index... done.")
